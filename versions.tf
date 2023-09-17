e4ba0bad56cb9b8b118cd15c774b2c7c603ee70a
machamp0714 <machamp0714@gmail.com>
2023-09-17 22:53:23 +0900

    Config

---
+++ versions.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = "= 1.5.7"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.17.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "ap-northeast-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+
+  assume_role {
+    role_arn = var.role_arn
+  }
+}
