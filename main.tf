3a3dd9314253a7ed319c0ad416fd6224a9e66621
machamp0714 <machamp0714@gmail.com>
2023-09-18 20:28:17 +0900

    add: Atlantis

---
+++ main.tf
@@ -0,0 +1,24 @@
+module "atlantis" {
+  source  = "terraform-aws-modules/atlantis/aws"
+  version = "~> 3.0"
+
+  name = "atlantis"
+
+  # VPC
+  cidr            = "10.20.0.0/16"
+  azs             = ["ap-northeast-1a", "ap-northeast-1d"]
+  private_subnets = ["10.20.1.0/24", "10.20.2.0/24"]
+  public_subnets  = ["10.20.101.0/24", "10.20.102.0/24"]
+
+  # DNS
+  route53_zone_name = "machamp.blog"
+
+  # Atlantis
+  atlantis_repo_allowlist        = ["github.com/machamp0714/atlantis-demo/*"]
+  atlantis_github_app_id         = var.atlantis_github_app_id
+  atlantis_github_app_key        = file("./github_app_key.pem")
+  atlantis_github_webhook_secret = var.atlantis_github_webhook_secret
+
+  # ECS
+  ecs_fargate_spot = true
+}
--- variables.tf
+++ variables.tf
@@ -1,3 +1,5 @@
 variable "access_key" {}
 variable "secret_key" {}
 variable "role_arn" {}
+variable "atlantis_github_app_id" {}
+variable "atlantis_github_webhook_secret" {}
